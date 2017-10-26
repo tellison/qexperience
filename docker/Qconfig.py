4e1a9ee69021b2f14005a5d718f62a586ecaa38b
Tim Ellison <t.p.ellison@gmail.com>
2017-10-26 20:19:43 +0100

    Getting as far as running the Jupyter notebook

--- docker/Qconfig.py
+++ docker/Qconfig.py
@@ -1,13 +1,23 @@
 # Before you can use the jobs API, you need to set up an access token.
 # Log in to the Quantum Experience. Under "Account", generate a personal 
-# access token. Replace "None" below with the quoted token string.
+# access token.
+#
+# Either:
+# 1) Replace "PUT_YOUR_API_TOKEN_HERE" below with the quoted token string.
 # Uncomment the APItoken variable, and you will be ready to go.
+# 2) Export your API token as an environment variable called "IBMQ_API_TOKEN".
 
-APItoken = "PUT_YOUR_API_TOKEN_HERE"
+import os
+
+#APItoken = "PUT_YOUR_API_TOKEN_HERE"
 
 config = {
   "url": 'https://quantumexperience.ng.bluemix.net/api'
 }
 
+if "IBMQ_API_TOKEN" in os.environ:
+   APIToken = os.environ["IBMQ_API_TOKEN"]
+
 if 'APItoken' not in locals():
-raise Exception("Please set up your access token. See Qconfig.py.")
+   raise Exception("Please set up your access token. See Qconfig.py.")
+
