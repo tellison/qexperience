a2548612e6f9e997685a632d898db050676bcee4
Tim Ellison <t.p.ellison@gmail.com>
2017-10-26 21:39:50 +0100

    typo in py and default cmd in docker

--- docker/Qconfig.py
+++ docker/Qconfig.py
@@ -5,7 +5,7 @@
 # Either:
 # 1) Replace "PUT_YOUR_API_TOKEN_HERE" below with the quoted token string.
 # Uncomment the APItoken variable, and you will be ready to go.
-# 2) Export your API token as an environment variable called "IBMQ_API_TOKEN".
+# 2) Export your API token as an environment variable called "QISKIT_API_TOKEN".
 
 import os
 
@@ -15,9 +15,9 @@ config = {
   "url": 'https://quantumexperience.ng.bluemix.net/api'
 }
 
-if "IBMQ_API_TOKEN" in os.environ:
-   APIToken = os.environ["IBMQ_API_TOKEN"]
-
 if 'APItoken' not in locals():
-   raise Exception("Please set up your access token. See Qconfig.py.")
+   if 'QISKIT_API_TOKEN' in os.environ:
+      APItoken = os.environ['QISKIT_API_TOKEN']
+   else:
+      raise Exception("Please set up your access token. See Qconfig.py.")
 
