ca7a61fc5268fad183a7ce6f4ef1588b83870462
Tim Ellison <t.p.ellison@gmail.com>
2017-10-27 09:42:33 +0100

    Tidy up var name and docker port

--- docker/Qconfig.py
+++ docker/Qconfig.py
@@ -5,7 +5,7 @@
 # Either:
 # 1) Replace "PUT_YOUR_API_TOKEN_HERE" below with the quoted token string.
 # Uncomment the APItoken variable, and you will be ready to go.
-# 2) Export your API token as an environment variable called "QISKIT_API_TOKEN".
+# 2) Export your API token as an environment variable called "IBMQ_API_TOKEN".
 
 import os
 
@@ -16,8 +16,8 @@ config = {
 }
 
 if 'APItoken' not in locals():
-   if 'QISKIT_API_TOKEN' in os.environ:
-      APItoken = os.environ['QISKIT_API_TOKEN']
+   if 'IBMQ_API_TOKEN' in os.environ:
+      APItoken = os.environ['IBMQ_API_TOKEN']
    else:
       raise Exception("Please set up your access token. See Qconfig.py.")
 
